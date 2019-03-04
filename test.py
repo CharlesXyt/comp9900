6aadb883d44bd75e4623889afba1074218ed79ef
allsetformove <39544484+allsetformove@users.noreply.github.com>
2019-03-04 19:43:04 +1100

    Demostration for WK3
    Simple demo, including search bar, course detail page, and placeholders for learning outcomes of each course

---
+++ test.py
@@ -0,0 +1,75 @@
+from flask import Flask,render_template,request,redirect,session
+
+app = Flask(__name__) 
+app.secret_key = 'u2jksidjflsduwerjl'
+app.debug = True
+USER_DICT = {
+    0: {'course_code':'COMP9021','course_name':'Principles of programming'},
+    1: {'course_code':'COMP9900','course_name':'Infomation Technology Project'},
+    2: {'course_code':'COMP9336','course_name':'Mobile Data Networking'},
+    3:{'course_code':'COMP9334','course_name':'Capacity Planning of Computer Systems and Networks'},
+}
+
+
+#main page
+@app.route('/', methods=['GET'])
+def hello():
+    return render_template('main.html')
+
+
+@app.route('/placeholder')
+def temp():
+    cid = request.args.get('search')                #cid is something like COMP9021 now
+    i = 0
+    while i< len(USER_DICT):
+        if cid == USER_DICT[i]['course_code']:
+            return render_template('placeholder.html',info = USER_DICT[i]['course_code'])
+        i = i+1
+    return render_template('placeholder.html',info = 'wrong info page')
+
+
+# login system
+@app.route('/login',methods=['GET',"POST"])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    user = request.form.get('user') # get what POST passed
+    pwd = request.form.get('pwd') # get what POST passed
+    if user == 'admin' and pwd == '123456':
+        # all user info in one session
+        session['user_info'] = user
+        return redirect('/index')
+    else:
+        return render_template('login.html',msg ='Wrong username or password')
+
+#page after logged in
+@app.route('/index',endpoint='n1')
+def index():
+    user_info = session.get('user_info')
+    if not user_info:
+        return redirect('/login')
+
+    return render_template('index.html',user_dict = USER_DICT)
+
+#detail
+@app.route('/detail')
+def detail():
+    user_info = session.get('user_info')
+    if not user_info:
+        return redirect('/login')
+
+    uid = int(request.args.get('uid'))             #uid is something like 3 now
+    info = USER_DICT[uid]
+
+    return render_template('detail.html',info = info)
+
+
+#logout-delete session
+@app.route('/logout')
+def logout():
+    del session['user_info']
+    return redirect('/login')
+
+
+if __name__ == '__main__':
+    app.run()

1472830e163fc5ed72acec26733eb91d8b1303d0
CharlesXyt <Charles.yitong@gmail.com>
2019-03-23 16:41:15 +1100

    update for AWS comprehend

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from flask import render_template
 from upload_dataset import Course_Info
 from mongoengine import connect
 import json
+import re
 from boto3.session import Session
 
 app = Flask(__name__)
@@ -24,29 +25,26 @@ def generate():
             aws_key = f.readline().split(":")[1]
             aws_secret = f.readline().split(":")[1]
         return render_template("mainpage.html",course_list=course_list)
-    if request.method =="POST":
+    if request.method =="GET":
         course_id = request.form.get("course_info").split(" - ")
         connect("course_info")
-        for e in Course_Info.objects:
-            course_info = json.loads(e.to_json())
-            # print(course_info)
-            if course_info["_id"] == course_id:
-                print(course_info["_id"])
-                session = Session(aws_access_key_id=aws_key,
-                                  aws_secret_access_key=aws_secret,
-                                  region_name="ap-southeast-2")
-                client = session.client("comprehend")
-                try:
-                    cc = client.batch_detect_key_phrases(
-                        TextList=[
-                            course_info["description"]
-                        ],
-                        LanguageCode='en'
-                    )
-                    return render_template("mainpage.html", course_list=cc["ResultList"][0]["KeyPhrases"])
-                except Exception:
-                    return jsonify("error message",404)
-        return jsonify("no such course in database",404)
+        try:
+            result = Course_Info.objects(course_code=course_id)
+            description = json.loads(result.to_json())[0]["description"]
+            description = re.sub(r"[\n]"," ",description)
+            session = Session(aws_access_key_id=aws_key,
+                              aws_secret_access_key=aws_secret,
+                              region_name="ap-southeast-2")
+            client = session.client("comprehend")
+            cc = client.batch_detect_key_phrases(
+                TextList=[
+                    description
+                ],
+                LanguageCode='en'
+            )
+            return render_template("mainpage.html", course_list=cc["ResultList"][0]["KeyPhrases"])
+        except Exception:
+                return jsonify("error message"),404
 
 
 if __name__ == '__main__':
